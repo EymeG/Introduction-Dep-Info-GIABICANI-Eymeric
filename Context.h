ae58675b1e0439ce680383898a2853c3b8c6e0a7
EymeG <eymeric.giabicani8@etu.univ-lorraine.fr>
2023-07-05 16:38:55 +0200

    Context.h update

--- Context.h
+++ Context.h
@@ -14,6 +14,12 @@ typedef struct Context {
   int capacity_particles;
   Particle* particles;
 
+  // Solids
+  // Moving solids registration;
+
+  int num_solids;
+  Solid* solids;
+
   // Ground colliders 
   // Static spheres registration;
   int num_ground_sphere;
@@ -33,6 +39,11 @@ typedef struct Context {
   // Dynamic Constraints registration;
   int num_dynamic_constraint;
   InteractionConstraint* inter_constraint;
+
+  // Solid Constraint;
+  // Solid Constraints registration;
+  int num_solid_constraint;
+  SolidConstraint* solid_constraint;
   
 } Context;
 
@@ -41,15 +52,19 @@ Context* initializeContext(int capacity); // Initialize the simulation context
 
 void addParticle(Context* (context), float x, float y, float radius, float mass, int draw_id); // Add a particle to the simulation Context 
 
+void addSolid(Context* context, float x, float y, float radius, float mass, int draw_id, int partnumb, int distance); // Add Solid data to the simulation Context (NON FONCTIONNEL)
+
 //---------------------------
 
-// GET OBJECTS_ID METHODS
+// GET OBJECTS METHODS
 
 Particle getParticle(Context* context, int id); // Return the particle's data from context whose particle_id is id.
 
 SphereCollider getGroundSphereCollider(Context* (context), int id); // Return the SphereCollider's data from context whose SphereCollider_id is id.
 
 GroundCollider getGroundLine(Context* (context), int id); // Return the GroundCollider's data from context whose GroundCollider_id is id.
+
+Solid getSolid(Context* context, int id); // Return the Solid data from context whose Solid_id is id
 
 // ------------------------------------------------
 
@@ -69,7 +84,9 @@ void checkContactWithPlane(Context* context,int particle_id); // Check if the pa
 
 void checkContactWithSphere(Context* (context),int particle_id); // Check if the particle from context whose particle_id is particle_id is in contact with a SphereCollider of context and save Constraint data if True
 
-void checkDynamicConstraint(Context* context,int particle_id); // Check if the particle from context whose particle_id is particle_id is in contact with another particle of context and savec Constraint data if True
+void checkDynamicConstraint(Context* context,int particle_id); // Check if the particle from context whose particle_id is particle_id is in contact with another particle of context and save Constraint data if True
+
+void checkSolidConstraint(Context* context, int particle_id);  // Check if a particle from context whose particle_id is particle_id is part of a solid and save Constraint data if True
 
 // ------------------------------------------------
 
@@ -84,6 +101,7 @@ void dampVelocities(Context* (context));
 void updateExpectedPosition(Context* (context), float dt); 
 void addDynamicContactConstraints(Context* (context)); // Update the dynamic Contact Constraints register
 void addStaticContactConstraints(Context* (context)); // Update the static Contact Constraints register 
+void addSolidConstraint(Context* (context)); // Update the Solid Constraints register (NON TESTE)
 void projectConstraints(Context* (context)); // Calculate and add the effect of Contact constraint to position of all particles from context
 void updateVelocityAndPosition(Context* (context), float dt); // Update velocity and position of all context's particles after calculation of all constraints and external forces effects.
 void applyFriction(Context* (context));
